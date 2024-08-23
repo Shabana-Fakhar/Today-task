0f50401fbaea8b0bc29b8f05a0a2452e1643f318
Shabana-Fakhar <shabanafakhar0861@gmail.com>
2024-08-23 13:28:57 +0500

    first commit

---
+++ loops.js
@@ -0,0 +1,13 @@
+var index=0;
+while(index<=5){
+  console.log(index+"is true")
+   index++
+}
+//console.log(index)
+//var index=0;
+//do{
+    //console.log(index+"is true")
+   //index++
+//} while(index<=5)
+   // console.log(index)
+
